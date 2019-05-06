a173a14798a847f0e8b61cd9a1169fdf3cee7b8c
Ryan Munro (Monbrey) <rsm999@uowmail.edu.au>
2019-05-06 12:23:48 +1000

    Maybe actually fixed now though

--- src/cache.js
+++ src/cache.js
@@ -9,6 +9,7 @@ const { on, once, off, emit } = new EventEmitter()
 class Cache {
     constructor(options) {
         this.cache = new Cacheman('cachegoose-cache', options)
+        this.keys = []
     }
 }
 
@@ -19,7 +20,7 @@ Object.defineProperties(Cache.prototype, {
     emit: { value: emit },
     keys: {
         value: function() {
-            return this.cache.keys()
+            return this.keys
         }
     },
     get: {
@@ -30,18 +31,21 @@ Object.defineProperties(Cache.prototype, {
     set: {
         value: function(key, value, ttl, cb = noop) {
             if (ttl === 0) ttl = -1
+            this.keys.push[key]
             return this.cache.set(key, value, ttl, cb)
         }
     },
     del: {
         value: function(key, cb = noop) {
             this.emit('del', key)
+            this.keys = this.keys.filter(k => k !== key)
             return this.cache.del(key, cb)
         }
     },
     clear: {
         value: function(cb = noop) {
             this.emit('clear')
+            this.keys = []
             return this.cache.clear(cb)
         }
     }
